c0625c6ea083dcd36979046e2fb4c42dabfc113e
dhz <dhz@port.finance>
2022-01-29 14:39:22 +0800

    fmt

--- tests/mock-oracles.spec.ts
+++ tests/mock-oracles.spec.ts
@@ -83,7 +83,16 @@ describe("Test Mock Oracles", () => {
       pythPriceKeypair.publicKey
     );
     pythPriceRecord = parsePriceData(pythData.data);
-    assert(pythPriceRecord.price === price);
+    assert(pythPriceRecord.price === price*2);
+
+    await writeAccountWrapper.writePythPrice(pythPriceKeypair, {
+      slot: new anchor.BN(slot*2),
+    });
+    pythData = await provider.connection.getAccountInfo(
+      pythPriceKeypair.publicKey
+    );
+    pythPriceRecord = parsePriceData(pythData.data);
+    assert(pythPriceRecord.validSlot.toString() === (slot*2).toString());
   });
 
   it("Write SwitchBoard Data", async () => {
@@ -96,13 +105,31 @@ describe("Test Mock Oracles", () => {
       price: new anchor.BN(price),
       slot: new anchor.BN(slot),
     });
-    const switchboardPrice = await loadZeroCopyAggregator(
+    let switchboardPrice = await loadZeroCopyAggregator(
       provider.connection,
       switchBoardKeypair.publicKey
     );
     assert(switchboardPrice.result.result.toString() === price.toString());
     assert(
       switchboardPrice.result.roundOpenSlot.toString() === slot.toString()
+    );
+    await writeAccountWrapper.writeSwitchboardPrice(switchBoardKeypair, 1, {
+      price: new anchor.BN(price*2),
+    });
+    switchboardPrice = await loadZeroCopyAggregator(
+      provider.connection,
+      switchBoardKeypair.publicKey
+    );
+    assert(switchboardPrice.result.result.toString() === (price*2).toString());
+    await writeAccountWrapper.writeSwitchboardPrice(switchBoardKeypair, 1, {
+      slot: new anchor.BN(slot*2),
+    });
+    switchboardPrice = await loadZeroCopyAggregator(
+      provider.connection,
+      switchBoardKeypair.publicKey
+    );
+    assert(
+      switchboardPrice.result.roundOpenSlot.toString() === (slot*2).toString()
     );
   });
 });
